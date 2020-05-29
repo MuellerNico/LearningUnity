4c5357e43c45be311825f770745b625691f60be4
Nico Müller <muellernico@outlook.com>
2020-05-29 18:57:56 +0200

    2nd learning project. Experimented with forces, cubes and surface.

---
+++ Learning2/Assets/CameraMovement.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    public Transform player;
+    public Transform cam;
+    Vector3 offset = new Vector3(0, 1, -4);
+    
+    // Update is called once per frame
+    void Update()
+    {
+        cam.position = player.position + offset;
+    }
+}
---
+++ Learning2/Assets/playerMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour
+{
+
+    public Rigidbody rb;
+    public float forwardSpeed = 200f;
+    public float sideForce = 200f;
+
+    // Update is called once per frame
+    void Start()
+    {
+        rb.velocity = new Vector3(0, 0, forwardSpeed);
+    }
+    void Update()
+    {
+        rb.AddForce(0, 0, forwardSpeed, ForceMode.VelocityChange);
+
+        if (Input.GetKey("a")) {
+            rb.AddForce(-sideForce, 0, 0, ForceMode.VelocityChange);
+        }
+        if (Input.GetKey("d")) {
+            rb.AddForce(sideForce, 0, 0, ForceMode.VelocityChange);
+        }
+    }
+}
