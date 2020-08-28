9dc2c353689a1a6bb7a8a6f748f36528cf3f6728
Nico Müller <muellernico@outlook.com>
2020-08-28 15:14:54 +0200

    Level layout. Changed movement force. Added score.

---
+++ Learning2/Assets/Scripts/PlayerCollision.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
+
+public class PlayerCollision : MonoBehaviour
+{
+
+    public PlayerMovement movement;
+    public Canvas gameOverScreen;
+    public Text scoreText;
+    public Text finalScoreText;
+    public Transform player;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.tag == "Obstacle"){
+            movement.enabled = false;
+            gameOverScreen.enabled = true;
+            scoreText.enabled = false;
+            finalScoreText.text = "FINAL SCORE\n" + player.position.z.ToString("0");
+
+        }
+    }
+}
---
+++ Learning2/Assets/Scripts/Score.cs
@@ -0,0 +1,17 @@
+﻿
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public Transform player;
+    public Text ScoreText;
+
+    // Update is called once per frame
+    void Update()
+    {
+        ScoreText.text = player.position.z.ToString("0");
+    }
+}
